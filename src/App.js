808f2b04bb5f669f933d5075894d95138297d573
Naveed Janmohamed <naveed.janmohamed@icloud.com>
2020-06-30 13:59:47 +0100

    added search bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 const Header = () => (
   <div className="header grid">
@@ -6,12 +6,49 @@ const Header = () => (
   </div>
 );
 
-function App() {
-  return (
-    <div className="page">
-      <Header />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "",
+    };
+  }
+
+  handleChange = (event) => {
+    const { value } = event.target;
+    this.setState((prevState, props) => ({
+      ...prevState,
+      searchTerm: value,
+    }));
+    if (value.length > 2) {
+    }
+  };
+
+  handleKeyPress = (event) => {
+    // character limit search (2)
+    const { value } = event.target;
+    if (value.length > 2 && event.key === "Enter") {
+      alert(`search for ${value}`);
+    }
+  };
+
+  render() {
+    const { searchTerm } = this.state;
+    return (
+      <div className="page">
+        <Header />
+        <div className="search grid">
+          <input
+            className="input grid-item"
+            placeholder="Type something"
+            onChange={this.handleChange}
+            onKeyPress={this.handleKeyPress}
+            value={searchTerm}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
